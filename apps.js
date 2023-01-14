2ebaae52e2da5c0ed120d3a2f4d8bc2b0fae6414
José Victor Capistrano Dias <zevictorrsp@gmail.com>
2023-01-14 09:42:57 -0300

    feat: add active and inactive button on click

--- apps.js
+++ apps.js
@@ -1,21 +1,36 @@
-function btn1() 
-{
-    for (var i = 0; i < btn1.length; i++) {
-        btn1[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
-        });
+const btn1 = document.querySelector('.btn1');
+const btn2 = document.querySelector('.btn2');
+const btn3 = document.querySelector('.btn3');
+const btn4 = document.querySelector('.btn4');
+const btn5 = document.querySelector('.btn5');
+
+const BUTTONS = {
+  btn1,
+  btn2,
+  btn3,
+  btn4,
+  btn5,
+}
+
+function handleInactiveButtons(buttonName) {
+  for (const key in BUTTONS) {
+    if (key !== buttonName) {
+      BUTTONS[key].classList.remove("active");
     }
   }
+}
 
-function btn2(){}
-function btn3(){} 
-function btn4(){} 
-function btn5(){}
+function handleActive(buttonName) {
+  BUTTONS[buttonName].classList.add("active");
+}
 
+function handleClick(buttonName) {
+  handleActive(buttonName);
+  handleInactiveButtons(buttonName);
+}
 
-
-
-
-
+btn1.addEventListener('click', () => handleClick('btn1'))
+btn2.addEventListener('click', () => handleClick('btn2'))
+btn3.addEventListener('click', () => handleClick('btn3'))
+btn4.addEventListener('click', () => handleClick('btn4'))
+btn5.addEventListener('click', () => handleClick('btn5'))
